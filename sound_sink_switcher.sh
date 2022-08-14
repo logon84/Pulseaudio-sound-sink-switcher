e61715a6ef56a57c309e9374adb67024bb8d6711
logon84 <logon84@users.noreply.github.com>
2022-08-14 17:51:08 +0200

    Update sound_sink_switcher.sh

--- sound_sink_switcher.sh
+++ sound_sink_switcher.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+##!/bin/bash
 
 #list sinks: pactl list short sinks
 SINK1="alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1"
@@ -11,6 +11,15 @@ ACTIVE_SINK_ID=$(pacmd info | sed -n '/sink(/,/source(/p' | grep '*' | cut -d ' 
 NEXT_SINK=$(pactl list short sinks | grep "$SINK1\|$SINK2" | grep -v ^"$ACTIVE_SINK_ID")
 NEXT_ID=${NEXT_SINK::1}
 
+if [[ $NEXT_SINK == *$SINK1* ]]
+then
+	OUTPUT_NAME=$SINK1
+else
+	OUTPUT_NAME=$SINK2
+fi
+
 #switch to sink
 $(pacmd set-default-sink $NEXT_ID)
 echo "switching to sink $NEXT_ID"
+
+notify-send AudioSwitch "Switching to $OUTPUT_NAME"
